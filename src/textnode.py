731df4312d88f0890b4f9b556e84206596a35451
vpatrickv <patrickdelange68@gmail.com>
2025-06-17 22:35:13 +0200

    Added split_nodes_delimiter function

--- src/textnode.py
+++ src/textnode.py
@@ -44,4 +44,46 @@ def text_node_to_html_node(text_node):
         case TextType.IMAGE:
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case default:
-            raise TypeError("argument does not conmtain a valid TextType")
+            raise TypeError("argument does not contain a valid TextType")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type == TextType.NORMAL:
+            nodes = old_node.text.split(delimiter)
+            if len(nodes) % 2 == 0:
+                raise Exception("no matching closing delimiter has been found")
+            count = -1
+            for node in nodes:
+                count += 1
+                if node == "":
+                    continue
+                if count % 2 == 0:
+                    new_nodes.append(TextNode(node, TextType.NORMAL))
+                else:
+                    new_nodes.append(TextNode(node, text_type))
+        else:
+            new_nodes.append(old_node)
+    return new_nodes
+
+
+def boot_split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
